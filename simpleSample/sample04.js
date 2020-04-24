a75454e2bf85bf36926c71ba2da137c17d953bfc
KenMatsutaka <k.matsutaka.biz@gmail.com>
2020-04-25 06:59:24 +0900

    クラスの実装サンプル

---
+++ simpleSample/sample04.js
@@ -0,0 +1,14 @@
+/** メンバクラス */
+class Member {
+    // コンストラクタ
+    constructor(name) {
+        this.name = name;
+    }
+    // 名前を取得する
+    getName() {
+        return this.name;
+    }
+}
+// インスタンス生成
+let member1 = new Member("まつけん");
+console.log(member1.getName());
